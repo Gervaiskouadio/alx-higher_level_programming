1f040404462c51f4de0c4c5d14c93e4fd5712174
Gervaiskouadio <lelagervais1@gmail.com>
2023-06-12 19:13:37 -0700

    Tasks 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
-// prints the first argument passed to it
+const args = process.argv.slice(2);
 
-if (process.argv[2] === undefined) {
-  console.log('No argument');
+if (args.length === 0) {
+  console.log("No argument");
 } else {
-  console.log(process.argv[2]);}
+  console.log(args[0]);
+}
