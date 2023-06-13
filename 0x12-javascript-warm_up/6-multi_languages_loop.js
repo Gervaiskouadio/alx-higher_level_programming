428fe1f4a59d3a6cdea40de26946c7b69e214284
Gervaiskouadio <lelagervais1@gmail.com>
2023-06-12 20:06:32 -0700

    JavaScript - Warm up

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,8 +2,8 @@
 /* prints 3 lines: (like 1-multi_languages.js) but by
  using an array of string and a loop */
 
-const langs = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const lines = ["C is fun", "Python is cool", "JavaScript is amazing"];
 
-for (let i = 0; i < 3; i++) {
-  console.log(langs[i]);
+for (let i = 0; i < lines.length; i++) {
+  console.log(lines[i]);
 }
