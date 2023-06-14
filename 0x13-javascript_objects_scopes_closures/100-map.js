db2f42454226117ae031153c15f6d451fe2bc374
Gervaiskouadio <lelagervais1@gmail.com>
2023-06-13 17:48:26 -0700

    JavaScript - Objects, Scopes and Closures

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,4 +1,7 @@
 #!/usr/bin/node
-const list = require('./100-data.js').list;
-console.log(list);
-console.log(list.map((item, index) => item * index));
+import { list } from './100-data.js';
+
+const newList = list.map((value, index) => value * index);
+
+console.log("Initial list:", list);
+console.log("New list:", newList);
